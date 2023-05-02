7e4b546cc2d309e35f215f961b070a498be2e10c
John Chong <johnchong.99.17@gmail.com>
2023-05-02 14:04:59 -0500

    work on part 1-2

---
+++ models.py
@@ -0,0 +1,64 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Site user."""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    username = db.Column(
+        db.String(20),
+        nullable=False,
+        unique=True)
+
+    password = db.Column(
+        db.String(100),
+        nullable=False)
+
+    email = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+#CLASS METHODS FOR REGISTERING USER
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(pwd).decode('utf8')
+
+        # return instance of user w/username and hashed pwd
+        return cls(username=username,
+                   password=hashed,
+                   email=email,
+                   first_name=first_name,
+                   last_name=last_name)
+
